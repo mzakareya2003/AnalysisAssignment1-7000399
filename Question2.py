e6e06ae7ec9d545766abe3b3f44c10bc2087e8dc
mzakareya2003 <82609018+mzakareya2003@users.noreply.github.com>
2023-11-03 23:46:31 +0100

    Update Question2.py

--- Question2.py
+++ Question2.py
@@ -2,3 +2,14 @@
 Mohamed Ihab Seifeldin Zakaria 
 7000399 / T-2
 """
+#2(a)
+
+#2(b)
+# THEORETICALLY merge sort time complexity is o(nlog(n))
+# AND binary search time complexity is O(log(n)) 
+# therefore our program using both of these sequentially
+# would prove time complexity of O(nlog(n))+O(log(n)) which 
+# AND the complexity is the dominant therefore it is theoretically O(nlog(n))
+
+#2(c)
+##explained in the report
